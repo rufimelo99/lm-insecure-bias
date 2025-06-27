b7010ca3f8a10751d2588876a311000838c558c0
Rui Melo <rufimelo99@gmail.com>
2025-06-27 14:01:49 +0200

    Update dpo.py

--- sec_aware_cl/alignment/dpo.py
+++ sec_aware_cl/alignment/dpo.py
@@ -247,8 +247,6 @@ def main(model, directory, output_dir):
 
                     snippets.append(data)
 
-                    break
-
             alignemnt_dict[cwe].append(
                 {
                     "aligned_count": cwe_aligned_count,
