933fd966b850582c87de370c2df7a1e246590bfc
Rui Melo <rufimelo99@gmail.com>
2025-06-28 00:35:32 +0200

    Update schemas.py

--- sec_aware_cl/schemas.py
+++ sec_aware_cl/schemas.py
@@ -6,6 +6,8 @@ class AvailableModels(str, Enum):
     GPT2 = "openai-community/gpt2"
     LLAMA_INSTRUCT = "meta-llama/Llama-3.2-3B-Instruct"
     code_llama = "meta-llama/CodeLlama-7b-hf"
+    code_llama_13b = "meta-llama/CodeLlama-13b-hf"
+    code_llama_70b = "meta-llama/CodeLlama-70b-hf"
     vicuna7 = "lmsys/vicuna-7b-v1.5"
     vicuna13 = "lmsys/vicuna-13b-v1.5"
     dscoder7 = "deepseek-ai/deepseek-coder-6.7b-base"
